a2d5fdd7b57b319eb4ccacdc358404fcd47995d5
BojanDabevski <bojan.dabevski@live.com>
2022-04-24 22:45:46 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Navigate, Route} from 'react-router-dom'
+import React, {Component} from "react";
+import BooksList from "./components/BookList";
+import Header from "./components/Header";
+import BookRepository from "./repo/BookRepository";
+import BookAdd from "./components/BookAdd";
+import BookEdit from "./components/BookEdit";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      authors: [],
+      selectedBook: {}
+    }
+  }
+
+  render() {
+    return (
+        <Router>
+          <Header/>
+          <main>
+            <div className="container">
+              <Route path={"/book/add"} exact render={() =>
+                  <BookAdd authors={this.state.authors}
+                           onAddBook={this.addBook}/>}
+              />
+              <Route path={"/book/edit/:id"} exact render={() =>
+                  <BookEdit authors={this.state.authors}
+                            onEditBook={this.editBook}
+                            book={this.state.selectedBook}/>}/>
+
+
+              <Route path={"/books"} exact render={() =>
+                  <BooksList books={this.state.books}
+                             onDelete={this.deleteBook}
+                             onEdit={this.getBook}
+                             onMarkAsTaken={this.markAsTaken}/>}
+              />
+              <Navigate to={"/books"}/>
+            </div>
+          </main>
+        </Router>
+    );
+  }
+
+  componentDidMount() {
+    this.loadBooks();
+    this.loadAuthors();
+  }
+
+  loadBooks = () => {
+    BookRepository.fetchBooks()
+        .then((data) => {
+          this.setState({
+            books: data.data
+          })
+        });
+  }
+
+  loadAuthors = () => {
+    BookRepository.fetchAuthors()
+        .then((data) => {
+          this.setState({
+            authors: data.data
+          })
+        });
+  }
+
+  addBook = (name, category, authorId, availableCopies) => {
+    BookRepository.addBook(name, category, authorId, availableCopies)
+        .then(() => {
+          this.loadBooks();
+        });
+  }
+
+  deleteBook = (id) => {
+    BookRepository.deleteBook(id)
+        .then(() => {
+          this.loadBooks();
+        });
+  }
+
+  editBook = (id, name, category, authorId, availableCopies) => {
+    BookRepository.editBook(id, name, category, authorId, availableCopies)
+        .then(() => {
+          this.loadBooks();
+        });
+  }
+
+  getBook = (id) => {
+    BookRepository.getBookById(id)
+        .then((data) => {
+          this.setState({
+            selectedBook: data.data
+          })
+        })
+  }
+
+  markAsTaken = (id) => {
+    BookRepository.markAsTaken(id)
+        .then(() => {
+          this.loadBooks();
+        });
+  }
+
+
+
 }
 
 export default App;
